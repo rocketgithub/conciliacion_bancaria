6df27949393f902cd5993374f5293f8c5a74fc7a
rocketgithub <rodrigo@solucionesprisma.com>
2019-05-29 12:49:54 -0600

    No importar inicialmente

--- report/__init__.py
+++ report/__init__.py
@@ -3,5 +3,5 @@
 import reporte_banco
 import asistente_reporte_banco
 import asistente_reporte_disponibilidad
-import reporte_disponibilidad_resumen
+#import reporte_disponibilidad_resumen
 # import banco_reporte_asistente
